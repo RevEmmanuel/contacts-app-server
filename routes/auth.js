dd54ffeaa3c558ecfa35148cef39977ef6e2839e
RevEmmanuel <adeolaae1@gmail.com>
2023-10-01 17:03:09 +0100

    edited verify user endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,7 @@ authRouter.post('/login', async (req, res, next) => {
     }
 })
 
-authRouter.post('/verify/:otp', async (req, res, next) => {
+authRouter.get('/verify/:otp', async (req, res, next) => {
     try {
         const otp = req.params.otp;
         if (!otp) {
