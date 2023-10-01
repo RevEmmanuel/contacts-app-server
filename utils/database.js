1639881bbd97a1e27c5002bf7b7419569407ede1
RevEmmanuel <adeolaae1@gmail.com>
2023-10-01 16:38:20 +0100

    added db port config

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { dialect: 'postgres' });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { dialect: 'postgres', port: process.env.DB_PORT });
 
 const testDbConnection = async () => {
     try {
