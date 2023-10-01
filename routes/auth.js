21e2957233aa9a161af3654cf8a309fb32943335
RevEmmanuel <adeolaae1@gmail.com>
2023-10-01 10:19:34 +0100

    added validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,26 @@
 const {createNewUser, loginUser, verifyUser} = require("../service/authService");
 const Router = require("express");
+const yup = require('yup');
+const InvalidOtpException = require("../exceptions/InvalidOtpException");
 
 const authRouter = Router();
 
+// validation schema
+const createUserRequest = yup.object().shape({
+    username: yup.string().required('Please enter a username').min(2, 'Please enter a valid username'),
+    email: yup.string().email().required('Please enter an email').min(2, 'Please enter a valid email'),
+    password: yup.string().required('Please enter a password').min(8, 'Password must be a minimum of 8 characters')
+});
+
+const loginRequest = yup.object().shape({
+    email: yup.string().email().required('Please enter an email').min(2, 'Please enter a valid email'),
+    password: yup.string().required('Please enter a password').min(8, 'Password must be a minimum of 8 characters')
+});
+
 authRouter.post('/signup', async (req, res, next) => {
+    const requestBody = req.body;
+    await createUserRequest.validate(requestBody);
     try {
-        const requestBody = req.body;
         const createdUser = await createNewUser(requestBody);
         const createUserResponse = {
             id: createdUser.id,
@@ -22,7 +37,7 @@ authRouter.post('/signup', async (req, res, next) => {
 
 authRouter.post('/login', async (req, res, next) => {
     const loginDto = req.body;
-    console.log(loginDto);
+    await loginRequest.validate(loginDto);
     try {
         const token = await loginUser(loginDto);
         res.status(200).json({ token });
@@ -34,6 +49,9 @@ authRouter.post('/login', async (req, res, next) => {
 authRouter.post('/verify/:otp', async (req, res, next) => {
     try {
         const otp = req.params.otp;
+        if (!otp) {
+            throw new InvalidOtpException('Please provide an otp');
+        }
         await verifyUser(otp);
         res.status(200).json({ message: 'User verified successfully' });
     } catch (error) {
